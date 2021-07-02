3b6249967da6c3207f283edb78c2636443219081
Dimitar <dconev@gmail.com>
2021-07-02 13:33:41 +0100

    prepare for deploy

--- project/settings.py
+++ project/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 import os
 from pathlib import Path
+import django_on_heroku
 from dotenv import load_dotenv
 import dj_database_url
 load_dotenv()
@@ -161,3 +163,5 @@ REST_FRAMEWORK = {
 
 
 AUTH_USER_MODEL = 'jwt_auth.User'
+
+django_on_heroku.settings(locals())
